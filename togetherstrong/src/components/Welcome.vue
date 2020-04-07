413c759ba412bca065404e07e3304e924117d593
Borja Penuelas <bmpenuelas@gmail.com>
2020-04-07 15:20:07 +0200

    Remove unused components

---
+++ togetherstrong/src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Welcome">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- togetherstrong/src/views/About.vue
+++ togetherstrong/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
+    <!-- TODO: Add about -->
+    <!-- TODO: Add TOS -->
     <h1>This is an about page</h1>
   </div>
 </template>
